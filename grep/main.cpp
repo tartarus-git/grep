51edade589d90839f00c8fe9393f2723f2b5bb7a
tartarus-git <niktedig@gmail.com>
2021-10-22 10:56:33 +0200

    piping wasn't working because somehow the ANSI escape codes disable it. Fixed that by doing that virtual terminal processing conditionally. Added some more basic boilerplate stuff and color handling and such.

--- grep/main.cpp
+++ grep/main.cpp
@@ -25,21 +25,47 @@
 #define fileno(x) _fileno(x)
 #endif
 
+bool usingColors = false;
+
 namespace color {
 	char* red;
+	char* reset;
 
 	//const char* red = ANSI_ESCAPE_CODE_PREFIX "31" ANSI_ESCAPE_CODE_SUFFIX;
 	//const char* green  = ANSI_ESCAPE_CODE_PREFIX "32" ANSI_ESCAPE_CODE_SUFFIX;
 	//const char* reset = ANSI_ESCAPE_CODE_PREFIX "0" ANSI_ESCAPE_CODE_SUFFIX;
 }
 
+namespace format {
+	char* error;
+	char* endl;
+}
+
+void releaseColorStrings() {
+	if (usingColors) {
+		delete[] color::red;
+	}
+}
+
+void releaseFormatStrings() {
+	delete[] format::error;
+}
+
+// Shows help text.
+void showHelp() {
+	std::cout << format::error << "help text not implemented" << format::endl;
+	exit(EXIT_SUCCESS);
+	// TODO: implement
+}
+
+// args validation
 void manageArgs(int argc, char** argv) {
 	switch (argc) {
 	case 1:
-		// Too little
-		return;
+		std::cout << format::error << "too few arguments" << format::endl;
+		showHelp();
 	case 2:
-		// Just right
+		
 		return;
 	default:
 		// Too much
@@ -48,28 +74,48 @@ void manageArgs(int argc, char** argv) {
 }
 
 int main(int argc, char** argv) {
-#ifdef PLATFORM_WINDOWS
-	HANDLE consoleOutputHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-	if (!consoleOutputHandle || consoleOutputHandle == INVALID_HANDLE_VALUE) { exit(EXIT_FAILURE); }
-	DWORD mode;
-	if (!GetConsoleMode(consoleOutputHandle, &mode)) { exit(EXIT_FAILURE); }
-	if (!SetConsoleMode(consoleOutputHandle, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING)) { exit(EXIT_FAILURE); }
+	// Only enable virtual terminal processing if stdout is a tty. If we're piping, don't do that.
+	if (isatty(fileno(stdout))) {
+		usingColors = true;
+
+		color::red = new char[ANSI_ESCAPE_CODE_MIN_SIZE + 2 + 1];
+		memcpy(color::red, ANSI_ESCAPE_CODE_PREFIX "31" ANSI_ESCAPE_CODE_SUFFIX, ANSI_ESCAPE_CODE_MIN_SIZE + 2 + 1);
+
+		color::reset = new char[ANSI_ESCAPE_CODE_MIN_SIZE + 1 + 1];
+		memcpy(color::reset, ANSI_ESCAPE_CODE_PREFIX "0" ANSI_ESCAPE_CODE_SUFFIX, ANSI_ESCAPE_CODE_MIN_SIZE + 1 + 1);
+	}
+	else {
+		color::red = new char[1]; color::red[0] = '\0';
+		color::reset = new char[1]; color::reset[0] = '\0';
+
+#ifdef PLATFORM_WINDOWS			// On windows, you have to set up virtual terminal processing explicitly and it for some reason disables piping.
+								// If we pipe, we need it enabled and we don't need ANSI key codes, so only do this when we don't pipe.
+		HANDLE consoleOutputHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+		if (!consoleOutputHandle || consoleOutputHandle == INVALID_HANDLE_VALUE) { return EXIT_FAILURE; }
+		DWORD mode;
+		if (!GetConsoleMode(consoleOutputHandle, &mode)) { return EXIT_FAILURE; }
+		if (!SetConsoleMode(consoleOutputHandle, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING)) { return EXIT_FAILURE; }
 #endif
 
-	// Check if stdout is pipe or not.
-	if (isatty(fileno(stdout))) { color::red = new char[ANSI_ESCAPE_CODE_MIN_SIZE + 2 + 1]; memcpy(color::red, ANSI_ESCAPE_CODE_PREFIX "31" ANSI_ESCAPE_CODE_SUFFIX, ANSI_ESCAPE_CODE_MIN_SIZE + 2 + 1); }
-	else { color::red = new char[1]; color::red[0] = '\0'; }
+	}
 
-	//std::cout << color::red << "ERROR: bruuuuh" << std::endl;
+	// TODO: Make this allocation react to if the colors are being done or not, because right now it doesn't.
 
-	//std::string hi;
-	//std::cin >> hi;
-	//std::cout << hi << std::endl;
-	//std::cout << "hi" << std::endl;
-	char thing[100];
-	fgets(thing, 100, stdin);
-	printf(thing);
-	printf("hi there");
+	// Allocate format strings.
+	format::error = new char[ANSI_ESCAPE_CODE_MIN_SIZE + 2 + 7 + 1];
+	memcpy(format::error, color::red, ANSI_ESCAPE_CODE_MIN_SIZE + 2);
+	memcpy(format::error + ANSI_ESCAPE_CODE_MIN_SIZE + 2, "ERROR: ", 8);	// This copies the null termination character as well because of 8.
 
-	delete[] color::red;
+	format::endl = new char[ANSI_ESCAPE_CODE_MIN_SIZE + 1 + 1 + 1];
+	memcpy(format::endl, color::reset, ANSI_ESCAPE_CODE_MIN_SIZE + 1);
+	memcpy(format::endl + ANSI_ESCAPE_CODE_MIN_SIZE + 1, "\n\0", 2);
+
+	// Main loop for searching through each line as it comes.
+	while (true) {
+
+	}
+
+	// TODO: Check if the pipe is empty and exit in that case, or else the user always has to interrupt, which is stupid.
+
+	releaseColorStrings();
 }
