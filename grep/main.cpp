4624d689315e3e2d5ee08d06a144d6ec4c426a65
tartarus-git <niktedig@gmail.com>
2021-10-23 17:08:22 +0200

    Added regex, made everything better, added flag functionality. Newline while piping doesn't work yet. Added sigint handler even though it works without because maybe not every shell is going to be nice enough to throw us an eof before siginting

--- grep/main.cpp
+++ grep/main.cpp
@@ -1,5 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS		// TODO: See if you can get away with not doing this.
-
 #ifdef PLATFORM_WINDOWS
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
@@ -7,20 +5,21 @@
 
 // TODO: Make sure the preprocessor defs in the setup menu are ordered, looks kind of weird right now.
 
-#include <iostream>
-
 #ifdef PLATFORM_WINDOWS
 #include <io.h>
 #else
 #include <unistd.h>
-#include <cstring>
 #endif
 
-#include <vector>
+#include <csignal>
 
-#define ANSI_ESCAPE_CODE_PREFIX "\033["
-#define ANSI_ESCAPE_CODE_SUFFIX "m"
-#define ANSI_ESCAPE_CODE_MIN_SIZE ((sizeof(ANSI_ESCAPE_CODE_PREFIX) - 1) + (sizeof(ANSI_ESCAPE_CODE_SUFFIX) - 1))
+#include <iostream>
+#include <string>
+#include <regex>
+
+#define ANSI_ESC_CODE_PREFIX "\033["
+#define ANSI_ESC_CODE_SUFFIX "m"
+#define ANSI_ESC_CODE_MIN_SIZE ((sizeof(ANSI_ESC_CODE_PREFIX) - 1) + (sizeof(ANSI_ESC_CODE_SUFFIX) - 1))
 
 // POSIX functions
 #ifdef PLATFORM_WINDOWS
@@ -28,112 +27,154 @@
 #define fileno(x) _fileno(x)
 #endif
 
-const char* helpText = "grep takes a series of lines as input and outputs lines from input that have the specified string in them.\n" \
+const char* helpText = "grep accepts text as input and outputs the lines from the input that have the specified keyphrase in them\n" \
 					   "\n" \
-					   "usage: grep <search string>";
+					   "usage: grep [-c] <search string>\n" \
+					   "\n" \
+					   "arguments:\n" \
+							"\t-c		-->			be case sensitive when matching";
 
-bool usingColors = false;
+bool isOutputColored;
 
 namespace color {
 	char* red;
+	void initRed() {
+		if (isOutputColored) {
+			red = new char[ANSI_ESC_CODE_MIN_SIZE + 2 + 1];
+			memcpy(red, ANSI_ESC_CODE_PREFIX "31" ANSI_ESC_CODE_SUFFIX, ANSI_ESC_CODE_MIN_SIZE + 2 + 1);
+			return;
+		}
+		red = new char;
+		*red = '\0';
+	}
+
 	char* reset;
+	void initReset() {
+		if (isOutputColored) {
+			reset = new char[ANSI_ESC_CODE_MIN_SIZE + 1 + 1];
+			memcpy(reset, ANSI_ESC_CODE_PREFIX "0" ANSI_ESC_CODE_SUFFIX, ANSI_ESC_CODE_MIN_SIZE + 1 + 1);
+			return;
+		}
+		reset = new char;
+		*reset = '\0';
+	}
 
-	//const char* red = ANSI_ESCAPE_CODE_PREFIX "31" ANSI_ESCAPE_CODE_SUFFIX;
-	//const char* green  = ANSI_ESCAPE_CODE_PREFIX "32" ANSI_ESCAPE_CODE_SUFFIX;
-	//const char* reset = ANSI_ESCAPE_CODE_PREFIX "0" ANSI_ESCAPE_CODE_SUFFIX;
-}
-
-void releaseColorStrings() {
-	delete[] color::red;
-	delete[] color::reset;
+	void release() {
+		delete[] color::red;
+		delete[] color::reset;
+	}
 }
 
 namespace format {
 	char* error;
+	void initError() {
+		if (isOutputColored) {
+			error = new char[ANSI_ESC_CODE_MIN_SIZE + 2 + 7 + 1];
+			memcpy(error, color::red, ANSI_ESC_CODE_MIN_SIZE + 2);
+			memcpy(error + ANSI_ESC_CODE_MIN_SIZE + 2, "ERROR: ", 7 + 1);
+			return;
+		}
+		error = new char[7 + 1];
+		memcpy(error, "ERROR: ", 7 + 1);
+	}
+
 	char* endl;
+	void initEndl() {
+		if (isOutputColored) {
+			endl = new char[ANSI_ESC_CODE_MIN_SIZE + 1 + 1 + 1];
+			memcpy(endl, color::reset, ANSI_ESC_CODE_MIN_SIZE + 1);
+			memcpy(endl + ANSI_ESC_CODE_MIN_SIZE + 1, "\n", 1 + 1);
+			return;
+		}
+		endl = new char[1 + 1];
+		memcpy(endl, "\n", 1 + 1);
+	}
+
+	void release() {
+		delete[] format::error;
+		delete[] format::endl;
+	}
 }
 
-void releaseFormatStrings() {
-	delete[] format::error;
-	delete[] format::endl;
+void releaseOutputStyling() {
+	format::release();
+	color::release();
 }
 
-void releaseIOStyleStrings() {
-	releaseFormatStrings();
-	releaseColorStrings();
+bool shouldLoopRun = true;
+void signalHandler(int signum) { shouldLoopRun = false; }
+
+namespace flags {
+	bool caseSensitive = false;
 }
 
-// Shows help text.
+void parseFlagGroup(char* arg) {
+	for (int i = 0; ; i++) {
+		switch (arg[i]) {
+		case 'c':
+			flags::caseSensitive = true;
+			break;
+		case '\0': return;
+		default:
+			color::initRed();
+			format::initError();
+			color::initReset();
+			format::initEndl();
+			std::cout << format::error << "one or more flag arguments is invalid" << format::endl;
+			releaseOutputStyling();
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+unsigned int parseFlags(int argc, char** argv) {
+	for (int i = 1; i < argc; i++) {
+		if (argv[i][0] == '-') { parseFlagGroup(argv[i] + 1); continue; }
+		return i;
+	}
+	return argc;				// No argument was found that wasn't a flag group.
+}
+
 void showHelp() {
 	std::cout << helpText << std::endl;
-	releaseIOStyleStrings();
+	releaseOutputStyling();
 	exit(EXIT_SUCCESS);
 }
 
-char* targetString;
-size_t targetString_len;
+std::regex keyphraseRegex;
 
-// args validation
 void manageArgs(int argc, char** argv) {
-	size_t actualLen;
-	switch (argc) {
-	case 1:
+	unsigned int keyphraseArgIndex = parseFlags(argc, argv);
+	switch (argc - keyphraseArgIndex) {
+	case 0:
+		color::initRed();
+		format::initError();
+		color::initReset();
+		format::initEndl();
 		std::cout << format::error << "too few arguments" << format::endl;
 		showHelp();
-	case 2:
-		targetString_len = strlen(argv[1]);
-		actualLen = targetString_len + 1;
-		targetString = new char[actualLen];
-		memcpy(targetString, argv[1], actualLen);
+	case 1:
+		{
+			std::regex_constants::syntax_option_type regexFlags = std::regex_constants::grep;
+			if (!flags::caseSensitive) { regexFlags |= std::regex_constants::icase; }
+			keyphraseRegex = std::regex(argv[keyphraseArgIndex], regexFlags);
+		}
 		return;
 	default:
+		color::initRed();
+		format::initError();
+		color::initReset();
+		format::initEndl();
 		std::cout << format::error << "too many arguments" << format::endl;
-		releaseIOStyleStrings();
+		releaseOutputStyling();
 		exit(EXIT_SUCCESS);
 	}
 }
 
-bool isTargetInString(const char* string, size_t stringLen, const char* target, size_t targetLen) {
-	if (targetLen > stringLen) { return false; }
-
-	for (int i = 0; i <= stringLen - targetLen; i++) {
-		if (!strncmp(string + i, target, targetLen)) {			// Why the hell do I have to put ! in front of this. Make sure this is the right way to do it.
-			return true;
-		}
-	}
-	return false;
-}
-
-std::string getLine() {			// TODO: This needs to be better.
-	std::vector<char> buffer;
-	while (true) {
-		int tempchar = getchar();
-		if (tempchar < 0) {
-			return "\0";
-		}
-		if (tempchar == '\n') {
-			if (buffer.size() == 0) {
-				continue;					// Skip all the empty lines because our system can't handle those anywhere other than here for now.
-			}
-			break;
-		}
-		buffer.push_back(tempchar);
-	}
-	buffer.push_back('\0');
-
-	char* anotherBuffer = new char[buffer.size()];
-	memcpy(anotherBuffer, buffer.data(), buffer.size());
-	
-	std::string result = anotherBuffer;
-	delete[] anotherBuffer;
-	return result;
-}
-
 int main(int argc, char** argv) {
-	// Only enable virtual terminal processing if stdout is a tty. If we're piping, don't do that.
-	if (isatty(fileno(stdout))) {
-		usingColors = true;
+	signal(SIGINT, signalHandler);
 
+	if (isatty(fileno(stdout))) {					// Only enable colors if stdout is a tty to make piping output easier.
 #ifdef PLATFORM_WINDOWS			// On windows, you have to set up virtual terminal processing explicitly and it for some reason disables piping.
 		// If we pipe, we need it disabled and we don't need ANSI key codes, so only do this when we don't pipe.
 		HANDLE consoleOutputHandle = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -142,46 +183,17 @@ int main(int argc, char** argv) {
 		if (!GetConsoleMode(consoleOutputHandle, &mode)) { return EXIT_FAILURE; }
 		if (!SetConsoleMode(consoleOutputHandle, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING)) { return EXIT_FAILURE; }
 #endif
-
-		color::red = new char[ANSI_ESCAPE_CODE_MIN_SIZE + 2 + 1];
-		memcpy(color::red, ANSI_ESCAPE_CODE_PREFIX "31" ANSI_ESCAPE_CODE_SUFFIX, ANSI_ESCAPE_CODE_MIN_SIZE + 2 + 1);
-
-		color::reset = new char[ANSI_ESCAPE_CODE_MIN_SIZE + 1 + 1];
-		memcpy(color::reset, ANSI_ESCAPE_CODE_PREFIX "0" ANSI_ESCAPE_CODE_SUFFIX, ANSI_ESCAPE_CODE_MIN_SIZE + 1 + 1);
-
-		// Allocate format strings.
-		format::error = new char[ANSI_ESCAPE_CODE_MIN_SIZE + 2 + 7 + 1];
-		memcpy(format::error, color::red, ANSI_ESCAPE_CODE_MIN_SIZE + 2);
-		memcpy(format::error + ANSI_ESCAPE_CODE_MIN_SIZE + 2, "ERROR: ", 8);	// This copies the null termination character as well because of 8.
-
-		format::endl = new char[ANSI_ESCAPE_CODE_MIN_SIZE + 1 + 1 + 1];
-		memcpy(format::endl, color::reset, ANSI_ESCAPE_CODE_MIN_SIZE + 1);
-		memcpy(format::endl + ANSI_ESCAPE_CODE_MIN_SIZE + 1, "\n", 2);				// This does too.
+		isOutputColored = true;
 	}
-	else {
-		color::red = new char[1]; color::red[0] = '\0';
-		color::reset = new char[1]; color::reset[0] = '\0';
-
-		format::error = new char[7 + 1];
-		memcpy(format::error, "ERROR: ", 7 + 1);		// This copies null termination along with the message.
-
-		format::endl = new char[1 + 1];
-		memcpy(format::error, "\n", 1 + 1);				// This does too.
-	}
+	else { isOutputColored = false; }
 
 	manageArgs(argc, argv);
 
 	// Main loop for searching through each line as it comes.
-	while (true) {
-		std::string inputString = getLine();
-		if (inputString.size() == 0) {
-			break;
-		}
-		if (isTargetInString(inputString.c_str(), inputString.size(), targetString, targetString_len)) {
-			std::cout << inputString << std::endl;
-		}
+	while (shouldLoopRun) {
+		std::string line;
+		std::getline(std::cin, line);
+		if (std::cin.eof()) { break; }
+		if (std::regex_search(line, keyphraseRegex)) { std::cout << line << std::endl; }
 	}
-
-	delete[] targetString;
-	releaseIOStyleStrings();
 }
