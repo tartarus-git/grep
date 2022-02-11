3f2d6835617167b8e29c03f2cd183d16bc4a0b2e
tartarus-git <niktedig@gmail.com>
2022-02-11 13:24:24 +0100

    Debugging conversion script.

--- test/convert_templates_to_ascii.ps1
+++ test/convert_templates_to_ascii.ps1
@@ -1,4 +1,4 @@
 Get-ChildItem ./templates | Foreach-Object {
     Get-Content $_.FullName | Out-File -FilePath ./tempfile.txt -Encoding ascii
-    cp ./tempfile.txt "./templates/$($_.FullName)"
+    cp ./tempfile.txt $_.FullName
 }
